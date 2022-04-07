d5e712f29c0075b5885e2d0ec36d897833bd1361
kyoko <kevin.kbr123@gmail.com>
2022-04-06 22:01:57 -0400

    Agregando la API y algo de estilos con Chakra ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+	fetchAllLaunches,
+	fetchLaunchesByFlightNumber
+} from "./services/launches_fetch";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 
 export const App = () => {
+	const [launches, setLaunches] = useState([]);
+	useEffect(() => {
+		fetchAllLaunches().then((data) => setLaunches(data));
+	}, []);
+
 	return (
-		<div>
-			<h1>Hola Mundo</h1>
-			<h2>Hola Mundo</h2>
-		</div>
+		<>
+			{/* <img src={logo} ></img> */}
+			<Heading as="h1" fontSize="3xl">
+				Spaces Launches
+			</Heading>
+			{!launches.length == 0 ? (
+				<section>
+					{launches.map((launch) => (
+						<Box
+							key={launch.flight_number}
+							bg="gray.100"
+							p="4"
+							m="4"
+							borderRadius="lg"
+						>
+							<Flex display="flex">
+								<Text fontSize="2xl">
+									Mission <strong> {launch.mission_name}</strong> (
+									{launch.launch_year})
+								</Text>
+							</Flex>
+              {launch.mission_name} ({launch.launch_year})
+						</Box>
+					))}
+				</section>
+			) : (
+				<h3>cargando...</h3>
+			)}
+		</>
 	);
 };
